40df27a1532603c8538e5d376ac0f2e453569f36
hao jen chung <hc073q@att.com>
2014-02-13 14:52:26 -0800

    import socket.io onto screenB.html

--- public/js/multiscreen.js
+++ public/js/multiscreen.js
@@ -1,6 +1,7 @@
 console.log("multiscreen.js is loaded.");
 
 var socket = io.connect('http://localhost:5000');
+
 socket.on('news', function(data) {
     console.log(data);
     socket.emit('my other event', {
