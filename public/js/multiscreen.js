a95e5d9d9812be29f7e1fd2be4ab964e509679ee
Sun, David (ds640c) <ds640c@att.com>
2014-02-13 21:15:43 -0800

    try to position new task as proportionately as possible. Bush can only go left. Palo Alto can only go right.

--- public/js/multiscreen.js
+++ public/js/multiscreen.js
@@ -1,5 +1,28 @@
 $(function() {
-	var office = $(".container").attr("id");
+	var offices = {
+		"paloAlto" : {
+			channel: "paloAlto",
+			emits: "paloAlto",
+			leftOffset: function () {
+				return 10;
+			},
+			shouldSwitch: function (left) {
+				return  left < -140;
+			}
+		},
+		"bush" : {
+			channel: "bush",
+			emits: "bush",
+			leftOffset: function () {
+				return $(".container").width() - 250;
+			},
+			shouldSwitch: function (left) {
+				return  left >  $(".container").width() - 140;
+			}
+		}
+	}
+	
+	var office = offices[$(".container").attr("id")];
 	var socket = io.connect('/');
    
 	var dragOptions = {
@@ -8,16 +31,13 @@ $(function() {
     		//console.log(event);
     	},
     	drag: function (event, ui) {
-    		var screenWidth = $(".container").width();
     		var $task = ui.helper;
-    		var shouldSwitch = function () {
-    			return  ui.position.left >  screenWidth - 140 || ui.position.left < -140;
-    		}
 
-    		if (shouldSwitch()) {
-    			socket.emit(office, {
+    		if (office.shouldSwitch(ui.position.left)) {
+    			socket.emit(office.emits, {
     				"id": $task.attr("id"),
-    				"description": $task.text().trim()
+    				"description": $task.text().trim(), 
+    				"top":   ui.position.top/$(".container").height()
     			});
     			$task.remove();
     		}
@@ -26,19 +46,22 @@ $(function() {
 
 	var createTask = function (task) {
 		if($("#" + task.id).length == 0) {
+			var $container = $(".container");
 			$newTask = $("<div id='" + task.id + "' class='task draggable ui-widget-content'>" + task.description + "</div>");
-			$(".container").append($newTask);
+			$newTask.css("top", $container.height() * task.top + "px");
+			$newTask.css("left", office.leftOffset() + "px");
 			$newTask.draggable(dragOptions);
+			$container.append($newTask);
 		}
 	}
 
-    $( ".draggable" ).draggable(dragOptions);
+    $(".draggable").draggable(dragOptions);
 
 	socket.on('connect', function () {
 		socket.emit('room', 'screens')
-	})
+	});
 
-	socket.on(office, function(data) {
+	socket.on(office.channel, function(data) {
 	    console.log(data);
 	    createTask(data);
 	});
