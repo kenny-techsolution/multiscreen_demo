95797dac5b522ebc16e1a50d4ec9e9b5840036c8
Sun, David (ds640c) <ds640c@att.com>
2014-02-13 17:48:50 -0800

    can drag task from Bush to Palo Alto

--- public/js/multiscreen.js
+++ public/js/multiscreen.js
@@ -1,26 +1,43 @@
-console.log("multiscreen.js is loaded.");
-
-var socket = io.connect('http://localhost:5000');
-
-socket.on('news', function(data) {
-    console.log(data);
-    socket.emit('my other event', {
-        my : 'data'
-    });
-});
-
-
 $(function() {
-    $( ".draggable" ).draggable({
+	var office = $(".container").attr("id");
+	var socket = io.connect('http://localhost:5000');
+	
+	var dragOptions = {
     	scroll: false,
     	create: function (event, ui ) {
     		//console.log(event);
     	},
     	drag: function (event, ui) {
-    		var screenWidth = $("#container").width();
-    		if (ui.position.left > screenWidth - 140) {
-    			ui.helper.hide();
+    		var screenWidth = $(".container").width();
+    		var $task = ui.helper;
+    		if ($task.is(":visible") && ui.position.left >  screenWidth - 140) {
+    			socket.emit(office, {
+    				"task": $task.attr("id"),
+    				"description": $task.text().trim()
+    			});
+    			$task.hide();
     		}
     	}
-    });
+	}
+	
+	var createTask = function (task) {
+		$newTask = $("<div id='" + task.id + "' class='task draggable ui-widget-content'>" + task.description + "</div>");
+		$(".container").append($newTask);
+		$newTask.draggable(dragOptions);
+	}
+	
+    $( ".draggable" ).draggable(dragOptions);
+	
+	socket.on('connect', function () {
+		socket.emit('room', 'screens')
+	})
+	
+	socket.on('paloAlto', function(data) { 
+	    console.log(data); 
+	    createTask(data);
+	});
+	
+	socket.on('bush', function(data) { 
+	    console.log(data); 
+	});
 });
